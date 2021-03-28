aca0bf965a6bb92dd37f6d53e6e9241d778c6540
Filipe Zabala <35049061+filipezabala@users.noreply.github.com>
2021-03-28 19:42:44 -0300

    Add files via upload

---
+++ corona-rs.R
@@ -0,0 +1,28 @@
+# working directory
+setwd(tempdir())
+getwd()
+
+# covid19
+download.file('https://ti.saude.rs.gov.br/covid19/download', 'temp.csv', 
+              mode =  'wb')
+c19 = read.csv('temp.csv', sep = ';')
+unlink('temp.csv')
+head(c19)
+str(c19)
+(tab = table(c19$FAIXAETARIA))
+par(mfrow=c(1,2))
+barplot(tab, main = 'Contaminados por faixa etária', xlab = 'Faixa etária',
+        sub = paste0('(Atulalizado em ', Sys.time(), ')') )
+
+# vacina
+download.file('https://vacina.saude.rs.gov.br/api/aplicacao', 'temp.csv', 
+              mode =  'wb')
+vac.apl = read.csv('temp.csv', sep = ';', encoding = 'latin1')
+unlink('temp.csv')
+head(vac.apl)
+str(vac.apl)
+(tab2 = table(vac.apl$FAIXAETARIA))
+round(prop.table(tab2), 3)
+barplot(tab2, main = 'Vacinação por faixa etária', xlab = 'Faixa etária',
+        sub = paste0('(Atulalizado em ', Sys.time(), ')') )
+
